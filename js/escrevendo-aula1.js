c9b202dd518aa99ec4d5710265396746c7fe95e9
oRodrigoRios <orodrigo.rios@gmail.com>
2022-06-30 11:04:06 -0300

    começo do fim

---
+++ js/escrevendo-aula1.js
@@ -0,0 +1,12 @@
+var button = document.querySelector("button");
+
+button.onclick = function () {
+    var nome = prompt("Qual é o comando para iniciar o git?");
+    if ((nome == "git init")) {
+        alert("Opa! " + nome + ", Parabéns! Você acertou!!");
+    }
+    else {
+        alert("Opa! " + nome + ", esse não é o comando correto para inicializar o Git.");
+    }
+    window.location="aula2.html";
+}
